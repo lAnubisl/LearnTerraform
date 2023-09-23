bb7e7a310a476b89318ab90acb7587b4d7a8fe5b
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2023-09-23 11:13:08 +0000

    update part _02_inputs

--- part_02_inputs/input.tfvars
+++ part_02_inputs/input.tfvars
@@ -1,2 +1,2 @@
 resource_group_name = "rg-learn-terraform-part-02"
-website_name = "my-static-website-2"
+website_name = "stapp-learn-terraform-part-02"
---
+++ part_02_inputs/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+   type = string
+   description = "The name of the resource group in which to create the resources."
+}
+
+variable "location" {
+   type = string
+   description = "The Azure Region in which to create the resources."
+   default = "westeurope"
+}
+
+variable "website_name" {
+   type = string
+   description = "The name of the web app."
+}
